9061fd311eda18d9c8b3a6f0830de41b95e81676
USERSAD\ecutilln <ecutilln@emeal.nttdata.com>
2022-06-08 11:09:56 +0200

    registro funcionando

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -5,28 +5,35 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
-
-  public signUpForm !: FormGroup
-  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
+  public signUpForm!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.signUpForm = this.formBuilder.group({
-      email: [""],
-      password: [""]
-    })
+      nombre: [''],
+      apellidos: [''],
+      email: [''],
+      password: [''],
+    });
   }
 
   signUp(){
-    this.http.post<any>("http://localhost:3000/signupUsersList",this.signUpForm.value)
-    .subscribe(res=>{
-      alert('SIGNIN SUCCESFUL');
+    this.http.post<any>("http://localhost:3000/users",this.signUpForm.value)
+    .subscribe(response=>{
+      alert('Registro correcto');
       this.signUpForm.reset()
       this.router.navigate(["login"])
-    },err=>{
-      alert("Something went wrong")
+    },error=>{
+      alert("Algo ha ido mal")
     })
   }
 }
+
+
