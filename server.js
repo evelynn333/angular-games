fcd8c66fbccf902dc86d71fdc6b973b0d9b5fa89
USERSAD\ecutilln <ecutilln@emeal.nttdata.com>
2022-06-15 11:12:56 +0200

    primer commit heroku

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/angular-games'));
+app.use(express.static(__dirname + "/dist/angular-games"));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-games/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/angular-games/index.html"));
 });
 
 // default Heroku port
